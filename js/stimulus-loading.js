18d8835195f675918a32edf5f870c08fcc3b265e
felix1251 <domhack123@gmail.com>
2023-12-27 07:45:03 +0800

    remove eager load function from stimulus loading js file

--- js/stimulus-loading.js
+++ js/stimulus-loading.js
@@ -3,35 +3,6 @@ import "@hotwired/stimulus";
 
 const controllerAttribute = "data-controller";
 
-// Eager load all controllers registered beneath the `under` path in the import map to the passed application instance.
-export function eagerLoadControllersFrom(under, application) {
-  const paths = Object.keys(parseImportmapJson()).filter((path) =>
-    path.match(new RegExp(`^${under}/.*_controller$`))
-  );
-  paths.forEach((path) => registerControllerFromPath(path, under, application));
-}
-
-function parseImportmapJson() {
-  return JSON.parse(document.querySelector("script[type=importmap]").text)
-    .imports;
-}
-
-function registerControllerFromPath(path, under, application) {
-  const name = path
-    .replace(new RegExp(`^${under}/`), "")
-    .replace("_controller", "")
-    .replace(/\//g, "--")
-    .replace(/_/g, "-");
-
-  if (canRegisterController(name, application)) {
-    import(path)
-      .then((module) => registerController(name, module, application))
-      .catch((error) =>
-        console.error(`Failed to register controller: ${name} (${path})`, error)
-      );
-  }
-}
-
 // Lazy load controllers registered beneath the `under` path in the import map to the passed application instance.
 export function lazyLoadControllersFrom(
   under,
